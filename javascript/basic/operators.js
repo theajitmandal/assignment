05b13886cde587b35daddac1ab8ff49648fb21e3
ajitdhanukmandal <theajitmandal@gmail.com>
2024-04-23 08:28:47 +0545

    added two constants and compared two constants

---
+++ javascript/basic/operators.js
@@ -0,0 +1,13 @@
+//Q1 create two constants, add them assign to a new constants
+const p = 10;
+const q = 5;
+const r = p + q;
+console.log(r);
+
+
+
+//Check below code, output should be false by comparing. console.log the answer
+const a = 10;
+const b = '10';
+
+console.log(a===b);
