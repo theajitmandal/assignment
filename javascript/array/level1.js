5046ad0ebc1b3df23f8c4bacfd5c2a7a1351bdec
ajitdhanukmandal <theajitmandal@gmail.com>
2024-04-24 19:26:38 +0545

    solved assignments of 24th April

--- javascript/array/level1.js
+++ javascript/array/level1.js
@@ -13,3 +13,26 @@ console.log(backupArr);
 ///Q3 join the array and console log
 // expected output 352
 console.log(arr.join(''));
+
+//Q4 Remove the last element from the array and log the modified array to the console
+const secondBackUpArr = [...arr];
+secondBackUpArr.pop();
+console.log(secondBackUpArr);
+
+// // Q5 Check if all elements in the num are even. Log "All even" or "Not all even" accordingly.
+const num = [6,4,2,8,10,12]
+
+let allEven = num.every(num => num%2===0);
+console.log(allEven);
+if(allEven){
+    console.log("All Even");
+}else{
+    console.log("Not All Even");
+}
+
+// // Find the index of a specific element in the array. Log the index to the console. 
+// //expected output: 3 //index should be 2
+
+const allNum = [3, 5, 2, 8, 5];
+const searchElement = 2;
+console.log(allNum.indexOf(2));
--- javascript/array/level2.js
+++ javascript/array/level2.js
@@ -2,6 +2,26 @@ const arr =  [3,5,2];
 //console.log the maximum number of an array
 //expectedd output 5
 
-
 const maxOfArr = Math.max(...arr);
 console.log(maxOfArr);
+
+//Q2 Remove a specific element 2 from the array. Log the modified array to the console.
+// expected output: [3,5]
+
+const backupArr = [...arr];
+const indexOfElementToRemove = backupArr.indexOf(2);
+backupArr.splice(indexOfElementToRemove, 1);
+console.log(backupArr);
+
+//Q3 Multiply each element of the array by a constant value 5 and log the modified array to the console.
+//expected output [15,10]
+const mulArr = backupArr.map((item) => {
+    return item*5;
+})
+console.log(mulArr);
+
+//find intersection (common elements) of arr and arr2
+//expected output: [5]
+const arr2 = [5,21,32];
+const interSection = arr.filter(item => arr2.includes(item));
+console.log(interSection);
